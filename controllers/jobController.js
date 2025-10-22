f5d4377786362f7b130927540d37dc294f2e1525
Nagendra-Akinepelly123 <nagendraakinepelly@gmail.com>
2025-10-22 18:24:26 +0530

    Implemented CRUD operations for JOBs

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,6 +1,9 @@
+import mongoose from "mongoose";
 import Job from "../models/jobModel.js";
+import moment from "moment";
 
-export const createJob = async (req, res, next) => {
+//CREATE JOBS LOGIC
+export const createJobController = async (req, res, next) => {
   const { company, position } = req.body;
 
   if (!company || !position) {
@@ -12,11 +15,118 @@ export const createJob = async (req, res, next) => {
   res.status(201).json({ job });
 };
 
-export const getJobs = async (req, res, next) => {
+//GET JOBS LOGIC
+export const getAllJobsController = async (req, res, next) => {
   const job = await Job.find({ createdBy: req.user.userId });
   res.status(200).send({
     success: true,
     totalJobs: job.length,
     job,
+  });
+};
+
+//UPDATE JOBS LOGIC
+export const updateJobController = async (req, res, next) => {
+  const { id } = req.params;
+  const { company, position } = req.body;
+
+  if (!company || !position) {
+    return next("please provide all fields");
+  }
+
+  const job = await Job.findOne({ _id: id });
+  if (!job) {
+    return next("Job Not found");
+  }
+  if (!req.user.userId === job.createdBy.toString()) {
+    return next("Your Not Authorized to update this job");
+  }
+
+  const updatedJob = await Job.findByIdAndUpdate({ _id: id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({ updatedJob });
+};
+
+//DELETE JOB ROUTE
+export const deleteJobController = async (req, res, next) => {
+  const { id } = req.params;
+
+  const job = await Job.findOne({ _id: id });
+  if (!job) {
+    return next(`No job Found with This ID ${id}`);
+  }
+  if (!req.user.userId === job.createdBy.toString()) {
+    return next("Your not Authorized to delete this job");
+  }
+
+  await Job.findByIdAndDelete({ _id: id });
+
+  res.status(200).json({ message: "Success Job is deleted" });
+};
+
+export const jobStatsController = async (req, res) => {
+  const stats = await Job.aggregate([
+    {
+      //stage-1
+      $match: {
+        createdBy: new mongoose.Types.ObjectId(req.user.userId), // Selects documents with this user ID
+        // status: { $ne: "reject" }, // And the status is not 'rejected'
+      },
+    },
+    {
+      $group: {
+        _id: "$status",
+        count: { $sum: 1 },
+      },
+    },
+  ]);
+
+  //default stats
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    reject: stats.reject || 0,
+  };
+
+  //monthly application
+  let monthlyApplications = await Job.aggregate([
+    {
+      $match: {
+        createdBy: new mongoose.Types.ObjectId(req.user.userId),
+      },
+    },
+    {
+      $group: {
+        _id: {
+          year: { $year: "$createdAt" },
+          month: { $month: "$createdAt" },
+        },
+        count: {
+          $sum: 1,
+        },
+      },
+    },
+  ]);
+
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format("MMM Y");
+      return { date, count };
+    })
+    .reverse();
+  res.status(200).json({
+    totalJobs: stats.length,
+    defaultStats,
+    monthlyApplications,
   });
 };
--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -2,12 +2,30 @@
 import express from "express";
 
 //files
-import { createJob, getJobs } from "../controllers/jobController.js";
+import {
+  createJobController,
+  deleteJobController,
+  getAllJobsController,
+  jobStatsController,
+  updateJobController,
+} from "../controllers/jobController.js";
 import userAuth from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/create-job", userAuth, createJob);
-router.get("/get-jobs", userAuth, getJobs);
+//GET JOBS || GET
+router.post("/create-job", userAuth, createJobController);
+
+//POST JOBS  || POST
+router.get("/get-jobs", userAuth, getAllJobsController);
+
+//UPDATE JOBS || PUT OR PATCH
+router.patch("/update-job/:id", userAuth, updateJobController);
+
+//DELETE JOBS || DELETE
+router.delete("/delete-job/:id", userAuth, deleteJobController);
+
+//JOB STATS || GET
+router.get("/job-stats", userAuth, jobStatsController);
 
 export default router;
